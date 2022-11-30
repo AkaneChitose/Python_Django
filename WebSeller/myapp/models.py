a80687561d537332fa04257d1e089770532105ab
Akane Chitose <katarina23122000@gmail.com>
2022-11-30 16:48:23 +0700

    UPDATE(hero file, database image,  Menu bar)

--- WebSeller/myapp/models.py
+++ WebSeller/myapp/models.py
@@ -9,3 +9,4 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.CharField(max_length=1000)
+    image = models.ImageField(blank=True, upload_to='images')
